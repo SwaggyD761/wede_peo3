69a6478b8a2f8867c8a3dc58eb0eee66665ff7d5
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:59:01 +0200

    Update account.php

--- librarian/account.php
+++ librarian/account.php
@@ -4,13 +4,174 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACCOUNT</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/accountStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="librarian-account">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon"></div>
+          <img
+            class="account-icon-container"
+            src="account-icon-container.png"
+          />
+        </div>
+        <div class="navbar-liase">
+          <div class="navbar-liase2"></div>
+          <div class="liase">LIASE</div>
+        </div>
+        <div class="navbar-database">
+          <div class="navbar-database2"></div>
+          <div class="database">DATABASE</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-verify">
+          <div class="navbar-verify2"></div>
+          <div class="verify">VERIFY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-container2"></div>
+    <div class="body-display">
+      <div class="body-container3"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="school-details">
+          <div class="school-details2"></div>
+          <div class="school-details-carousel">
+            <div class="carousel">
+              <div class="carousel-display">
+                <div class="detail-text-display"></div>
+                <div class="school-detail">username</div>
+                <div class="detail-text-display2"></div>
+                <div class="school-detail2">name</div>
+                <div class="detail-text-display3"></div>
+                <div class="school-detail3">surname</div>
+                <div class="detail-text-display4"></div>
+                <div class="school-detail4">email</div>
+              </div>
+            </div>
+            <div class="details">Details</div>
+          </div>
+          <div class="school-profile-display">
+            <div class="school-profit-display"></div>
+            <div class="school-image-container">
+              <div class="school-profile-image-display"></div>
+              <div class="school-image-display">
+                <div class="school-profile-image-display2"></div>
+              </div>
+            </div>
+            <div class="school-name-display">
+              <div class="school-name-display2"></div>
+              <div class="librarian-name">librarian name</div>
+            </div>
+            <div class="email-link">
+              <div class="link-container"></div>
+              <div class="email">email</div>
+            </div>
+          </div>
+        </div>
+        <div class="edit-details-carousel">
+          <div class="details-editcarousel"></div>
+          <div class="school-details3"></div>
+          <div class="details2">DETAILS</div>
+          <div class="carousel-display2">
+            <div class="detail-text-display"></div>
+            <div class="school-detail">name</div>
+            <div class="detail-text-display2"></div>
+            <div class="school-detail2">surname</div>
+            <div class="detail-text-display3"></div>
+            <div class="school-detail3">email</div>
+            <div class="detail-text-display4"></div>
+            <div class="school-detail4">cell</div>
+          </div>
+          <div class="carousel-display3">
+            <div class="detail-text-display"></div>
+            <div class="school-detail">street no</div>
+            <div class="detail-text-display2"></div>
+            <div class="school-detail2">street name</div>
+            <div class="detail-text-display3"></div>
+            <div class="school-detail3">city</div>
+            <div class="detail-text-display4"></div>
+            <div class="school-detail4">province</div>
+          </div>
+        </div>
+        <div class="files-table-ares">
+          <div class="files-table-area"></div>
+          <div class="files-table-header">
+            <div class="table-header"></div>
+            <div class="files">files</div>
+            <div class="table-contols">
+              <div class="left-button"></div>
+              <div class="right-button"></div>
+            </div>
+          </div>
+          <div class="table-display">
+            <div class="file-record">
+              <div class="record"></div>
+              <div class="print-icon">
+                <div class="print-option"></div>
+                <img class="print-icon2" src="print-icon2.png" />
+              </div>
+              <div class="aux-record-column">
+                <div class="value"></div>
+                <div class="_100-000-00">$ 100 000.00</div>
+              </div>
+              <div class="record-title">
+                <div class="loan-repayment-details"></div>
+                <div class="loan-my-m-ovember-deal">
+                  Loan: “My MOvember” Deal
+                </div>
+              </div>
+              <div class="record-index">
+                <div class="date"></div>
+                <div class="_06-04-2023">06/04/2023</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="loan-total-summary"></div>
+          <div class="account-details">Account Details</div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="profile-image"></div>
+          <div class="admin-name-display"></div>
+          <div class="name-surname">Name &amp; Surname</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
