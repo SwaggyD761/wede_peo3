dfc7b0adc21b395f5a1c3846f1b9a9d54b382b67
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:07:27 +0200

    Update home.php

--- users/home.php
+++ users/home.php
@@ -4,13 +4,141 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/homeStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="user-home">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon-container">
+            <div class="account-icon-container2"></div>
+            <img class="account-icon" src="account-icon.png" />
+            <div class="cart-icons">
+              <div class="cart-icon-container">
+                <div class="cart-icon-display"></div>
+                <img class="rectangle-97" src="rectangle-97.png" />
+              </div>
+              <div class="cart-amount-container">
+                <div class="cart-amount-display"></div>
+                <div class="_1">1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-chat">
+          <div class="navbar-chat2"></div>
+          <div class="chat">CHAT</div>
+        </div>
+        <div class="navbar-sell">
+          <div class="navbar-sell2"></div>
+          <div class="sell">SELL</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-buy">
+          <div class="navbar-buy2"></div>
+          <div class="buy">BUY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-container-layer"></div>
+    <div class="body-display">
+      <div class="body-display2"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="new-arrivals">
+          <div class="new-arrivals-display"></div>
+          <div class="new-arrivals2">New Arrivals</div>
+          <div class="books-display"></div>
+        </div>
+        <div class="hot-deals-announcements">
+          <div class="hot-deals-announcements-display"></div>
+          <div class="hot-deals">HOT DEALS</div>
+          <div class="hot-deals-carousel"></div>
+        </div>
+        <div class="notifications-display">
+          <div class="table-area"></div>
+          <div class="quick-buttons">
+            <div class="quick-buttons-container"></div>
+            <div class="sell-button">
+              <div class="sell-button2"></div>
+              <div class="sell2">SELL</div>
+            </div>
+            <div class="buy-button">
+              <div class="buy-button2"></div>
+              <div class="buy2">BUY</div>
+            </div>
+          </div>
+          <div class="notifications">
+            <div class="table-header">
+              <div class="table-header2"></div>
+              <div class="notifications2">Notifications</div>
+            </div>
+            <div class="table-display-carousel">
+              <div class="table-display"></div>
+              <div class="record">
+                <div class="record2"></div>
+                <div class="print-icon">
+                  <div class="print-option"></div>
+                  <img class="print-icon2" src="print-icon2.png" />
+                </div>
+                <div class="record-title">
+                  <div class="loan-repayment-details"></div>
+                  <div class="special-my-m-ovember-deal">
+                    Special: “My MOvember” Deal
+                  </div>
+                </div>
+                <div class="record-index">
+                  <div class="date"></div>
+                  <div class="_06-04-2023">06/04/2023</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="profile-display2"></div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="account-details">Account Details</div>
+          <div class="user-name-surname">
+            <div class="admin-name-display"></div>
+            <div class="name-surname">Name &amp; Surname</div>
+          </div>
+          <div class="profile-image"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
