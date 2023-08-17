f6499bd4be784ec1bc7fda024d8ba2f6ea1d1d8d
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:30:31 +0200

    Update librarian-login.php

--- librarian-login.php
+++ librarian-login.php
@@ -4,13 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/librarianLoginStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="librarian-login">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="nav-bar">
+      <div class="navbar"></div>
+      <div class="navbar-center">
+        <div class="navbar-site-title"></div>
+        <div class="library-app">LIBRARY APP</div>
+        <div class="navbar-center2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="body-conatiner">
+    <div class="body-display">
+      <div class="container"></div>
+      <div class="login-area">
+        <div class="rectangle-81"></div>
+        <div class="frame-68">
+          <div class="frame-67">
+            <div class="rectangle-82"></div>
+            <div class="login">LOGIN</div>
+          </div>
+          <div class="frame-66">
+            <div class="rectangle-83"></div>
+            <div class="frame-64">
+              <div class="rectangle-84"></div>
+              <div class="email">Email</div>
+            </div>
+            <div class="frame-65">
+              <div class="rectangle-85"></div>
+              <div class="password">Password</div>
+            </div>
+            <div class="submit-button"></div>
+            <div class="submit">SUBMIT</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
