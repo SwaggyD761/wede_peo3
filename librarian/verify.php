914753b309cc89f278b9c8371b1dcf304c2d9215
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:49:41 +0200

    Update verify.php

--- librarian/verify.php
+++ librarian/verify.php
@@ -3,14 +3,224 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GET CASH</title>
+    <title>VERIFY</title>
     <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="librarian-verify-books">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon"></div>
+          <img
+            class="account-icon-container"
+            src="account-icon-container.png"
+          />
+        </div>
+        <div class="navbar-liase">
+          <div class="navbar-liase2"></div>
+          <div class="liase">LIASE</div>
+        </div>
+        <div class="navbar-database">
+          <div class="navbar-database2"></div>
+          <div class="database">DATABASE</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-verify">
+          <div class="navbar-verify2"></div>
+          <div class="verify">VERIFY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="blur-layer"></div>
+    <div class="body-display">
+      <div class="body-display2"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="subjects">
+          <div class="table-area"></div>
+          <div class="table-display">
+            <div class="table-display2"></div>
+            <div class="record">
+              <div class="students-record"></div>
+              <div class="print-icon">
+                <div class="print-option"></div>
+                <img class="print-icon2" src="print-icon2.png" />
+              </div>
+              <div class="ax-record-column">
+                <div class="aux-record-column"></div>
+                <div class="_100-000-00">$ 100 000.00</div>
+              </div>
+              <div class="record-title">
+                <div class="record-title2"></div>
+                <div class="loan-my-m-ovember-deal">
+                  Loan: “My MOvember” Deal
+                </div>
+              </div>
+              <div class="record-index">
+                <div class="record-index2"></div>
+                <div class="_06-04-2023">06/04/2023</div>
+              </div>
+            </div>
+          </div>
+          <div class="table-header">
+            <div class="table-header2"></div>
+            <div class="user">User</div>
+          </div>
+        </div>
+        <div class="students">
+          <div class="table-area2"></div>
+          <div class="table-display3">
+            <div class="table-display2"></div>
+            <div class="record">
+              <div class="students-record"></div>
+              <div class="print-icon3">
+                <div class="print-option2"></div>
+                <img class="print-icon4" src="print-icon4.png" />
+              </div>
+              <div class="aux-record-column2">
+                <div class="aux-record-column"></div>
+                <div class="_100-000-00">$ 100 000.00</div>
+              </div>
+              <div class="record-title">
+                <div class="record-title2"></div>
+                <div class="loan-my-m-ovember-deal">
+                  Loan: “My MOvember” Deal
+                </div>
+              </div>
+              <div class="record-index">
+                <div class="record-index2"></div>
+                <div class="_06-04-2023">06/04/2023</div>
+              </div>
+            </div>
+          </div>
+          <div class="table-header3">
+            <div class="table-header2"></div>
+            <div class="book">Book</div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="loan-total-summary"></div>
+          <div class="account-details">Account Details</div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="profile-image"></div>
+          <div class="admin-name-display"></div>
+          <div class="name-surname">Name &amp; Surname</div>
+        </div>
+        <div class="profile-display">
+          <div class="loan-total-summary"></div>
+          <div class="account-details">Account Details</div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="profile-image"></div>
+          <div class="admin-name-display"></div>
+          <div class="name-surname">Name &amp; Surname</div>
+        </div>
+        <div class="verify-user">
+          <div class="table-controls"></div>
+          <div class="form-controls-2">
+            <div class="submit-decline-buttons">
+              <div class="decline-button">
+                <div class="submit-button"></div>
+                <div class="decline">DECLINE</div>
+              </div>
+              <div class="submit-button2">
+                <div class="submit-button"></div>
+                <div class="submit">SUBMIT</div>
+              </div>
+            </div>
+            <div class="search-results"></div>
+            <div class="search-record-in-students">
+              <div class="search-student">Search student:</div>
+              <div class="submit-button3"></div>
+              <div class="submit2">SUBMIT</div>
+            </div>
+            <div class="load-record-to-verify"></div>
+            <div class="load-user-to-verify">
+              <div class="loaded-user-to-verify"></div>
+              <div class="right-button-2">
+                <div class="rectangle-105"></div>
+                <div class="">&gt;</div>
+              </div>
+              <div class="left-button-1">
+                <div class="rectangle-104"></div>
+                <div class="2">&lt;</div>
+              </div>
+              <div class="user-name">USER NAME</div>
+            </div>
+          </div>
+          <div class="verify-user2">Verify user</div>
+        </div>
+        <div class="verify-books">
+          <div class="quick-admission"></div>
+          <div class="form-controls">
+            <div class="decline-book-submit-button">
+              <div class="submit-button4"></div>
+              <div class="decline-book">DECLINE BOOK</div>
+            </div>
+            <div class="load-book-submit-button">
+              <div class="submit-button4"></div>
+              <div class="load-book">LOAD BOOK</div>
+            </div>
+            <div class="condition-entry">
+              <div class="condition-dropdown"></div>
+              <div class="condition">Condition</div>
+            </div>
+            <div class="book-title-display">
+              <div class="book-title-text-display"></div>
+              <div class="book-title">Book title</div>
+            </div>
+            <div class="book-id-display">
+              <div class="book-id-text-display"></div>
+              <div class="book-id">Book Id</div>
+            </div>
+            <div class="load-book-to-verify">
+              <div class="loaded-book-to-verify-display"></div>
+              <div class="right-button">
+                <div class="rectangle-105"></div>
+                <div class="">&gt;</div>
+              </div>
+              <div class="left-button">
+                <div class="rectangle-104"></div>
+                <div class="2">&lt;</div>
+              </div>
+              <div class="book-name">BOOK NAME</div>
+            </div>
+          </div>
+          <div class="verify-book">verify-book</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
