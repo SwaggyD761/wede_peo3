0e2ee4ee32aec89567f0e5a2a2948d349d9046c4
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:28:12 +0200

    Add files via upload

---
+++ librarian/confirmation_pages/user-verification.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VERIFY USER</title>
+    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <style>
+        /* Inline CSS styles can also be defined here */
+    </style>
+</head>
+<body>
+    
+	
+	
+</body>
+</html>
