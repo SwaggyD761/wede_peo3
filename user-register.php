d7b0fbe506d345049bfc532d077f1f2b5b1f2308
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:27:55 +0200

    Update user-register.php

--- user-register.php
+++ user-register.php
@@ -4,13 +4,81 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REGISTER</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/userRegisterStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="register-user">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="nav-bar">
+      <div class="navbar"></div>
+      <div class="navbar-center">
+        <div class="navbar-site-title"></div>
+        <div class="library-app">LIBRARY APP</div>
+        <div class="navbar-center2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="body-conatiner">
+    <div class="body-display">
+      <div class="container"></div>
+      <div class="login-area">
+        <div class="rectangle-81"></div>
+        <div class="form-area">
+          <div class="form-header">
+            <div class="rectangle-82"></div>
+            <div class="sign-up">SIGN UP</div>
+          </div>
+          <div class="form">
+            <div class="rectangle-83"></div>
+            <div class="submit-button">
+              <div class="submit-button2"></div>
+              <div class="submit">SUBMIT</div>
+            </div>
+            <div class="form-data-entry">
+              <div class="confirm-password">
+                <div class="rectangle-85"></div>
+                <div class="confrim-password">Confrim Password</div>
+              </div>
+              <div class="password">
+                <div class="rectangle-852"></div>
+                <div class="password2">Password</div>
+              </div>
+              <div class="school">
+                <div class="school-entry"></div>
+                <div class="school2">School</div>
+              </div>
+              <div class="email">
+                <div class="email-entry"></div>
+                <div class="email2">Email</div>
+              </div>
+              <div class="cell-number">
+                <div class="cell-number-entry"></div>
+                <div class="cell-number2">Cell number</div>
+              </div>
+              <div class="id-number">
+                <div class="id-number-entry"></div>
+                <div class="id-number2">ID Number</div>
+              </div>
+              <div class="surname">
+                <div class="surname-entry"></div>
+                <div class="surname2">Surname</div>
+              </div>
+              <div class="name">
+                <div class="name-entry"></div>
+                <div class="name2">Name</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
