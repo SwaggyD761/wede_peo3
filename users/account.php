db0c221956150ad1dfd4b8821efc773a707d73d7
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:22:47 +0200

    Update account.php

--- users/account.php
+++ users/account.php
@@ -4,13 +4,231 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACCOUNT</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/accountStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="user-account">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon-container">
+            <div class="account-icon-container2"></div>
+            <img class="account-icon" src="account-icon.png" />
+            <div class="cart-icons">
+              <div class="cart-icon-container">
+                <div class="cart-icon-display"></div>
+                <img class="rectangle-97" src="rectangle-97.png" />
+              </div>
+              <div class="cart-amount-container">
+                <div class="cart-amount-display"></div>
+                <div class="_1">1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-chat">
+          <div class="navbar-chat2"></div>
+          <div class="chat">CHAT</div>
+        </div>
+        <div class="navbar-sell">
+          <div class="navbar-sell2"></div>
+          <div class="sell">SELL</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-buy">
+          <div class="navbar-buy2"></div>
+          <div class="buy">BUY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-container2"></div>
+    <div class="body-display">
+      <div class="body-container3"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="profile-and-tracking">
+          <div class="profile-and-tracking-container"></div>
+          <div class="orders-fullfilments">
+            <div class="orders-and-fullfillments"></div>
+            <div class="oder-and-fullfilments-display">
+              <div class="table-display">
+                <div class="record">
+                  <div class="rectangle-39"></div>
+                  <div class="print">
+                    <div class="print-option"></div>
+                    <img class="print-icon" src="print-icon.png" />
+                  </div>
+                  <div class="status">
+                    <div class="value"></div>
+                    <div class="_100-000-00">$ 100 000.00</div>
+                  </div>
+                  <div class="book-name">
+                    <div class="loan-repayment-details"></div>
+                    <div class="masterskill-software-testing">
+                      Masterskill Software Testing
+                    </div>
+                  </div>
+                  <div class="date">
+                    <div class="date2"></div>
+                    <div class="_06-04-2023">06/04/2023</div>
+                  </div>
+                </div>
+              </div>
+              <div class="tab-menu">
+                <div class="tab-menu2"></div>
+                <div class="orders">orders</div>
+                <div class="fullfilments">fullfilments</div>
+              </div>
+            </div>
+          </div>
+          <div class="profile-display">
+            <div class="profile-display2"></div>
+            <div class="quick-details-display"></div>
+            <div class="account-details">Account Details</div>
+            <div class="profile-name">
+              <div class="profile-name2"></div>
+              <div class="name-surname">name &amp; surname</div>
+            </div>
+            <div class="profile-image"></div>
+          </div>
+        </div>
+        <div class="cart">
+          <div class="loan-history-display"></div>
+          <div class="cart-buttons">
+            <div class="cart-footer">
+              <div class="footer-display"></div>
+              <div class="cart-checkout-button">
+                <div class="checkout-button"></div>
+                <div class="checkout">CHECKOUT</div>
+              </div>
+            </div>
+          </div>
+          <div class="credit-activity-record-display">
+            <div class="record-3">
+              <div class="rectangle-41"></div>
+              <div class="date3"></div>
+              <div class="loan-repayment-details2"></div>
+              <div class="value2"></div>
+              <div class="print-option2"></div>
+              <img class="print-icon2" src="print-icon2.png" />
+              <div class="_63-00">$ 63.00</div>
+              <div class="txb-392">TXB392</div>
+              <div class="c-programming-net-net-core-asp-net">
+                C# Programming: .Net .NetCore &amp; ASP.Net
+              </div>
+            </div>
+            <div class="record-2">
+              <div class="rectangle-40"></div>
+              <div class="date3"></div>
+              <div class="loan-repayment-details2"></div>
+              <div class="value2"></div>
+              <div class="print-option2"></div>
+              <img class="print-icon2" src="print-icon2.png" />
+              <div class="_37-00">$ 37.00</div>
+              <div class="txb-471">TXB471</div>
+              <div class="html-css-ph-p-programming-for-beginners">
+                Html, css &amp; PhP Programming for Beginners
+              </div>
+            </div>
+            <div class="record-1">
+              <div class="rectangle-392"></div>
+              <div class="loan-repayment-details2"></div>
+              <div class="value2"></div>
+              <div class="print-option2"></div>
+              <img class="print-icon2" src="print-icon2.png" />
+              <div class="_100-00">$ 100.00</div>
+              <div class="analytical-techniques">Analytical Techniques</div>
+              <div class="date3"></div>
+              <div class="txb-891">TXB891</div>
+            </div>
+          </div>
+          <div class="cart-header">
+            <div class="header-displa"></div>
+            <div class="cart2">CART</div>
+          </div>
+        </div>
+        <div class="account-information">
+          <div class="account-information-container"></div>
+          <div class="account-information-display">
+            <div class="sold-and-collected">
+              <div class="sold-collected-display"></div>
+              <div class="tab-display"></div>
+              <div class="tab-menu3">
+                <div class="tab-menu-display"></div>
+                <div class="history-tab-button">
+                  <div class="history">HISTORY</div>
+                </div>
+                <div class="sold-tab-button">
+                  <div class="sold">SOLD</div>
+                </div>
+              </div>
+            </div>
+            <div class="information-contrainer">
+              <div class="information-container"></div>
+              <div class="details-display-2">
+                <div class="user-information-display"></div>
+              </div>
+              <div class="header-status"></div>
+              <div class="school-detail">USERNAME</div>
+            </div>
+            <div class="carousel-display">
+              <div class="detail-text-display"></div>
+              <div class="school-detail2">Name</div>
+              <div class="detail-text-display2"></div>
+              <div class="school-detail3">Surname</div>
+              <div class="detail-text-display3"></div>
+              <div class="detail-text-display4"></div>
+              <div class="school-detail4">Email</div>
+              <div class="school-detail5">Cell</div>
+              <div class="detail-text-display5"></div>
+              <div class="school-detail6">School</div>
+            </div>
+            <div class="carousel-display2">
+              <div class="detail-text-display6"></div>
+              <div class="school-detail7">Street No</div>
+              <div class="detail-text-display7"></div>
+              <div class="school-detail8">Street Name</div>
+              <div class="detail-text-display8"></div>
+              <div class="school-detail9">City</div>
+              <div class="detail-text-display9"></div>
+              <div class="school-detail10">Province</div>
+            </div>
+          </div>
+          <div class="account-information2">Account Information</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
