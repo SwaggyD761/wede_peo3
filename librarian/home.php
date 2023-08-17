83c256a73d86146ee7521c3f85da727a339c0996
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:51:15 +0200

    Update home.php

--- librarian/home.php
+++ librarian/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/homeStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
