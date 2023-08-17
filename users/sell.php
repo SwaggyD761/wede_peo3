78877cb51ed81b5ff14c6753a4c1380946a5c242
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:20:47 +0200

    Update sell.php

--- users/sell.php
+++ users/sell.php
@@ -4,13 +4,182 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INVEST</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/sellStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="user-sell">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon-container">
+            <div class="account-icon-container2"></div>
+            <img class="account-icon" src="account-icon.png" />
+            <div class="cart-icons">
+              <div class="cart-icon-container">
+                <div class="cart-icon-display"></div>
+                <img class="rectangle-97" src="rectangle-97.png" />
+              </div>
+              <div class="cart-amount-container">
+                <div class="cart-amount-display"></div>
+                <div class="_1">1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-chat">
+          <div class="navbar-chat2"></div>
+          <div class="chat">CHAT</div>
+        </div>
+        <div class="navbar-sell">
+          <div class="navbar-sell2"></div>
+          <div class="sell">SELL</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-buy">
+          <div class="navbar-buy2"></div>
+          <div class="buy">BUY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-conatiner">
+    <div class="rectangle-86"></div>
+    <div class="body-display">
+      <div class="body-container"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="sales">
+          <div class="loan-history-display"></div>
+          <div class="credit-activity-display-header">
+            <div class="rectangle-38"></div>
+            <div class="files">Files</div>
+          </div>
+          <div class="credit-activity-record-display">
+            <div class="record-3">
+              <div class="rectangle-41"></div>
+              <div class="date"></div>
+              <div class="loan-repayment-details"></div>
+              <div class="value"></div>
+              <div class="print-option"></div>
+              <img class="print-icon" src="print-icon.png" />
+              <div class="_63-591-00">$ 63 591.00</div>
+              <div class="_26-11-2022">26/11/2022</div>
+              <div class="repayment">Repayment</div>
+            </div>
+            <div class="record-2">
+              <div class="rectangle-40"></div>
+              <div class="date"></div>
+              <div class="loan-repayment-details"></div>
+              <div class="value"></div>
+              <div class="print-option"></div>
+              <img class="print-icon" src="print-icon.png" />
+              <div class="_1-300-00">$ 1 300.00</div>
+              <div class="_18-02-2023">18/02/2023</div>
+              <div class="loan-affiliate-program-ref-985-fhdhfv">
+                Loan: Affiliate program (ref: 985fhdhfv)
+              </div>
+            </div>
+            <div class="record-1">
+              <div class="rectangle-39"></div>
+              <div class="loan-repayment-details"></div>
+              <div class="value"></div>
+              <div class="print-option"></div>
+              <img class="print-icon" src="print-icon.png" />
+              <div class="_100-000-00">$ 100 000.00</div>
+              <div class="loan-my-m-ovember-deal">Loan: “My MOvember” Deal</div>
+              <div class="date"></div>
+              <div class="_06-04-2023">06/04/2023</div>
+            </div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="profile-display2"></div>
+          <div class="quick-details-display"></div>
+          <div class="account-details">Account Details</div>
+          <div class="profile-name">
+            <div class="profile-name2"></div>
+            <div class="name-surname">name &amp; surname</div>
+          </div>
+          <div class="profile-image"></div>
+        </div>
+        <div class="sd">
+          <div class="sales-info-carousel">
+            <div class="rectangle-98"></div>
+          </div>
+          <div class="sell-book">
+            <div class="loan-repayment-application"></div>
+            <div class="credit-activity-display-header2">
+              <div class="rectangle-382"></div>
+              <div class="up">Up</div>
+            </div>
+            <div class="sell-book-form">
+              <div class="submit-button">
+                <div class="submit">SUBMIT</div>
+              </div>
+              <div class="indepth-book-details">
+                <div class="detail-text-display"></div>
+                <div class="detail-text-display2"></div>
+                <div class="detail-text-display3"></div>
+                <div class="detail-text-display4"></div>
+                <div class="detail-text-display5"></div>
+                <div class="school-detail">ISNB No</div>
+                <div class="school-detail2">Price</div>
+                <div class="school-detail3">Condition</div>
+                <div class="school-detail4">Edition</div>
+                <div class="school-detail5">Price</div>
+              </div>
+              <div class="quick-book-details"></div>
+              <div class="book-image">
+                <div class="frame-37"></div>
+                <div class="frame-75">
+                  <div class="upload-button"></div>
+                  <img class="rectangle-95" src="rectangle-95.png" />
+                </div>
+              </div>
+              <div class="carousel-display">
+                <div class="detail-text-display6"></div>
+                <div class="school-detail6">Title</div>
+                <div class="detail-text-display7"></div>
+                <div class="school-detail7">Author</div>
+                <div class="detail-text-display8"></div>
+                <div class="detail-text-display9"></div>
+                <div class="school-detail8">Author 2</div>
+                <div class="detail-text-display10"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
