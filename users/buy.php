059f64849b3fefa8d042e609a70fa63e4d882fbb
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:12:12 +0200

    Update buy.php

--- users/buy.php
+++ users/buy.php
@@ -4,13 +4,395 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GET CASH</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/buyStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="user-buy">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon-container">
+            <div class="account-icon-container2"></div>
+            <img class="account-icon" src="account-icon.png" />
+            <div class="cart-icons">
+              <div class="cart-icon-container">
+                <div class="cart-icon-display"></div>
+                <img class="rectangle-97" src="rectangle-97.png" />
+              </div>
+              <div class="cart-amount-container">
+                <div class="cart-amount-display"></div>
+                <div class="_1">1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-chat">
+          <div class="navbar-chat2"></div>
+          <div class="chat">CHAT</div>
+        </div>
+        <div class="navbar-sell">
+          <div class="navbar-sell2"></div>
+          <div class="sell">SELL</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-buy">
+          <div class="navbar-buy2"></div>
+          <div class="buy">BUY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="rectangle-13"></div>
+    <div class="rectangle-86"></div>
+    <div class="body-display">
+      <div class="body-display2"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="table-search">
+          <div class="tablr-search-display"></div>
+          <div class="search-result-display">
+            <div class="rectangle-100"></div>
+            <div class="result-record">
+              <div class="students-record"></div>
+              <div class="frame-95">
+                <div class="chack-availability">
+                  <div class="chack-availability-icon"></div>
+                  <img class="ask-icon" src="ask-icon.png" />
+                </div>
+                <div class="add-to-cart">
+                  <div class="add-to-cart2"></div>
+                  <img class="cart-icon" src="cart-icon.png" />
+                </div>
+                <div class="book-price">
+                  <div class="book-price-display"></div>
+                  <div class="_10-000-00">$ 10 000.00</div>
+                </div>
+              </div>
+              <div class="book-title">
+                <div class="book-title-display"></div>
+                <div class="successful-project-management">
+                  Successful project management
+                </div>
+              </div>
+              <div class="book-image">
+                <div class="book-image2"></div>
+                <img class="book-image3" src="book-image3.png" />
+              </div>
+            </div>
+          </div>
+          <div class="search-bar">
+            <div class="rectangle-99"></div>
+            <div class="rectangle-101"></div>
+            <img class="rectangle-102" src="rectangle-102.png" />
+          </div>
+          <div class="search-title">
+            <div class="search">SEARCH</div>
+          </div>
+        </div>
+        <div class="books-display">
+          <div class="table-area"></div>
+          <div class="table-display">
+            <div class="table-display2"></div>
+            <div class="row-seconday">
+              <div class="record">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record2">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record3">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record4">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="record">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record2">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record3">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+              <div class="record4">
+                <div class="students-record"></div>
+                <div class="frame-95">
+                  <div class="chack-availability">
+                    <div class="chack-availability-icon"></div>
+                    <img class="ask-icon" src="ask-icon.png" />
+                  </div>
+                  <div class="add-to-cart">
+                    <div class="add-to-cart2"></div>
+                    <img class="cart-icon" src="cart-icon.png" />
+                  </div>
+                  <div class="book-price">
+                    <div class="book-price-display"></div>
+                    <div class="_10-000-00">$ 10 000.00</div>
+                  </div>
+                </div>
+                <div class="book-title">
+                  <div class="book-title-display"></div>
+                  <div class="successful-project-management">
+                    Successful project management
+                  </div>
+                </div>
+                <div class="book-image">
+                  <div class="book-image2"></div>
+                  <img class="book-image3" src="book-image3.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="table-header">
+            <div class="table-header2"></div>
+            <div class="subjects">SUBJECTS</div>
+          </div>
+        </div>
+        <div class="books-catagory-display">
+          <div class="books-catagory-display2"></div>
+          <div class="catagory-controls">
+            <div class="catagory-buttons-display"></div>
+            <div class="left-button">
+              <div class="books-left"></div>
+              <div class="">&lt;</div>
+            </div>
+            <div class="right-button">
+              <div class="books-right"></div>
+              <div class="">&gt;</div>
+            </div>
+            <div class="frame-96">
+              <div class="rectangle-106"></div>
+              <div class="category-1">category 1</div>
+            </div>
+            <div class="frame-97">
+              <div class="rectangle-109"></div>
+              <div class="category-2">category 2</div>
+            </div>
+            <div class="frame-98">
+              <div class="rectangle-108"></div>
+              <div class="category-3">category 3</div>
+            </div>
+            <div class="frame-99">
+              <div class="rectangle-110"></div>
+              <div class="category-4">category 4</div>
+            </div>
+            <div class="frame-100">
+              <div class="rectangle-107"></div>
+              <div class="category-5">category 5</div>
+            </div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="loan-total-summary"></div>
+          <div class="frame-34">
+            <div class="det-progress-circle-display"></div>
+            <div class="rectangle-35"></div>
+            <div class="name-surname">name &amp; surname</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
