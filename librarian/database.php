0f289042a31e74406631887cef5a03a3d3373c89
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:52:33 +0200

    Update database.php

--- librarian/database.php
+++ librarian/database.php
@@ -4,13 +4,122 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INVEST</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/datbaseStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="librarian-database">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon"></div>
+          <img
+            class="account-icon-container"
+            src="account-icon-container.png"
+          />
+        </div>
+        <div class="navbar-liase">
+          <div class="navbar-liase2"></div>
+          <div class="liase">LIASE</div>
+        </div>
+        <div class="navbar-database">
+          <div class="navbar-database2"></div>
+          <div class="database">DATABASE</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-verify">
+          <div class="navbar-verify2"></div>
+          <div class="verify">VERIFY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-container2"></div>
+    <div class="body-display">
+      <div class="body-display2"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="controls">
+          <div class="controls2"></div>
+          <div class="submit-button"></div>
+          <div class="submit">SUBMIT</div>
+          <div class="record-key-entry"></div>
+          <div class="control-type-comnand-dropdown"></div>
+          <div class="type">Type</div>
+          <div class="controls3">Controls</div>
+        </div>
+        <div class="search">
+          <div class="search2"></div>
+          <div class="submit-button2"></div>
+          <div class="submit2">SUBMIT</div>
+          <div class="search-key-type-dropdown"></div>
+          <div class="record-key-entry2"></div>
+          <div class="type2">Type</div>
+          <div class="search3">Search</div>
+        </div>
+        <div class="profile-display">
+          <div class="loan-total-summary"></div>
+          <div class="account-details">Account Details</div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="profile-image"></div>
+          <div class="admin-name-display"></div>
+          <div class="name-surname">Name &amp; Surname</div>
+        </div>
+        <div class="database-table-interface">
+          <div class="database-table-area">
+            <div class="database-table-area2"></div>
+            <div class="table-header">
+              <div class="table-header2"></div>
+              <div class="database-name">TEXTBOOKS</div>
+            </div>
+            <div class="table-display">
+              <div class="table-display2"></div>
+            </div>
+          </div>
+          <div class="table-name-buttons">
+            <div class="database-selection-buttons"></div>
+            <div class="current-table-button-display"></div>
+            <div class="frame-91">
+              <div class="right-button"></div>
+              <div class="">&gt;</div>
+            </div>
+            <div class="frame-90">
+              <div class="left-button"></div>
+              <div class="2">&lt;</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
