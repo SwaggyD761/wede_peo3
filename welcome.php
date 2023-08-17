cda6422e1941d045ad19c919d3461c04fb5b334b
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:31:21 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,13 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WELCOME</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/welcomeStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="welcome">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="nav-bar">
+      <div class="navbar"></div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="navbar-site-title"></div>
+        <div class="library-app">LIBRARY APP</div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="container2"></div>
+    <div class="form-area">
+      <div class="rectangle-81"></div>
+      <div class="form-entry">
+        <div class="form-entry2"></div>
+        <div class="student-button">
+          <div class="student-button2"></div>
+          <div class="students">STUDENTS</div>
+        </div>
+        <div class="librarian-button">
+          <div class="school-button"></div>
+          <div class="librarian">LIBRARIAN</div>
+        </div>
+      </div>
+      <div class="form-header">
+        <div class="form-header2"></div>
+        <div class="welcome-msgs">
+          <div class="welcome2">WELCOME</div>
+          <div class="to-the-library">TO THE LIBRARY</div>
+          <div
+            class="the-no-1-library-app-that-allows-you-to-buy-and-sell-textbook-at-a-very-affordable-price"
+          >
+            THE NO.1 Library App That Allows You To <br />Buy And Sell Textbook
+            at a very affordable price
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
