5887493aab5f5a1a4fbe79a17c73bcd5b26314a6
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:55:42 +0200

    Update liase.php

--- librarian/liase.php
+++ librarian/liase.php
@@ -4,13 +4,145 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORUM</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/liaseStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="librarian-liase">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon"></div>
+          <img
+            class="account-icon-container"
+            src="account-icon-container.png"
+          />
+        </div>
+        <div class="navbar-liase">
+          <div class="navbar-liase2"></div>
+          <div class="liase">LIASE</div>
+        </div>
+        <div class="navbar-database">
+          <div class="navbar-database2"></div>
+          <div class="database">DATABASE</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-verify">
+          <div class="navbar-verify2"></div>
+          <div class="verify">VERIFY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-container2"></div>
+    <div class="body-display">
+      <div class="body-display2"></div>
+      <div class="content-layer">
+        <div class="content-layer2"></div>
+        <div class="messages">
+          <div class="messages2"></div>
+          <div class="next-button">
+            <div class="rectangle-33"></div>
+            <div class="next">Next</div>
+          </div>
+          <div class="prev-button">
+            <div class="rectangle-33"></div>
+            <div class="prev">Prev</div>
+          </div>
+          <div class="slider-display"></div>
+          <div class="slider-amount-display"></div>
+          <div class="from">from:</div>
+          <div class="messages3">Messages</div>
+        </div>
+        <div class="contact">
+          <div class="contact2"></div>
+          <div class="slider-display2"></div>
+          <div class="slider-amount-display2"></div>
+          <div class="to">To:</div>
+          <div class="contact3">Contact</div>
+          <div class="contact-submit-button">
+            <div class="rectangle-332"></div>
+            <div class="submit">SUBMIT</div>
+          </div>
+        </div>
+        <div class="notifications-table-area">
+          <div class="notifications-table-area2"></div>
+          <div class="credit-activity-record-display">
+            <div class="notification-records">
+              <div class="notification-records2"></div>
+              <div class="print-options">
+                <div class="print-option"></div>
+                <img class="print-icon" src="print-icon.png" />
+              </div>
+              <div class="aux-record-column">
+                <div class="value"></div>
+                <div class="_100-000-00">$ 100 000.00</div>
+              </div>
+              <div class="record-title">
+                <div class="loan-repayment-details"></div>
+                <div class="loan-my-m-ovember-deal">
+                  Loan: “My MOvember” Deal
+                </div>
+              </div>
+              <div class="record-index">
+                <div class="date"></div>
+                <div class="_06-04-2023">06/04/2023</div>
+              </div>
+            </div>
+          </div>
+          <div class="credit-activity-display-header">
+            <div class="rectangle-38"></div>
+            <div class="table-header">Notifications</div>
+          </div>
+        </div>
+        <div class="profile-display">
+          <div class="profile-display2"></div>
+          <div class="account-details">Account Details</div>
+          <div class="email-alerts-and-queries"></div>
+          <div class="profile-image"></div>
+          <div class="admin-name-display"></div>
+          <div class="name-surname">Name &amp; Surname</div>
+        </div>
+        <div class="announcements">
+          <div class="announcements2"></div>
+          <div class="submit-button"></div>
+          <div class="submit2">SUBMIT</div>
+          <div class="announcement-msg-entry"></div>
+          <div class="announcement-title-entry"></div>
+          <div class="title">Title:</div>
+          <div class="announcements3">Announcements</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
