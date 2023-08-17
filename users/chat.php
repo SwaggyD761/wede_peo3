1f188ac07b934af87aa5e3459f88085ff635f837
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:22:11 +0200

    Update chat.php

--- users/chat.php
+++ users/chat.php
@@ -4,13 +4,139 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORUM</title>
-    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <link rel="stylesheet" href="css/chatStyles.css"> <!-- Link to an external CSS file -->
     <style>
         /* Inline CSS styles can also be defined here */
     </style>
 </head>
 <body>
-    
+    <div class="user-chat">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="navbar">
+      <div class="navbar2"></div>
+      <div class="navbar-right">
+        <div class="navbar-right2"></div>
+        <div class="navbar-account">
+          <div class="navbar-account2"></div>
+          <div class="account-icon-container">
+            <div class="account-icon-container2"></div>
+            <img class="account-icon" src="account-icon.png" />
+            <div class="cart-icons">
+              <div class="cart-icon-container">
+                <div class="cart-icon-display"></div>
+                <img class="rectangle-97" src="rectangle-97.png" />
+              </div>
+              <div class="cart-amount-container">
+                <div class="cart-amount-display"></div>
+                <div class="_1">1</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-chat">
+          <div class="navbar-chat2"></div>
+          <div class="chat">CHAT</div>
+        </div>
+        <div class="navbar-sell">
+          <div class="navbar-sell2"></div>
+          <div class="sell">SELL</div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <div class="navbar-center2"></div>
+        <div class="site-tile">
+          <div class="navbar-site-title"></div>
+          <div class="library-app">LIBRARY APP</div>
+        </div>
+      </div>
+      <div class="navbar-left">
+        <div class="navbar-left2"></div>
+        <div class="navbar-buy">
+          <div class="navbar-buy2"></div>
+          <div class="buy">BUY</div>
+        </div>
+        <div class="navbar-home">
+          <div class="navbar-home2"></div>
+          <div class="home">HOME</div>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-menu2"></div>
+          <div class="hamburger-menu"></div>
+          <img class="rectangle-30" src="rectangle-30.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="rectangle-86"></div>
+  <div class="body-conatiner">
+    <div class="body-container"></div>
+    <div class="content-layer">
+      <div class="body-container2"></div>
+      <div class="conversations-area">
+        <div class="conversations-table-display"></div>
+        <div class="conversations-display-header">
+          <div class="conversations-display-header2"></div>
+          <div class="conversations">Conversations</div>
+        </div>
+        <div class="conversations-record-display">
+          <div class="conversation-record">
+            <div class="rectangle-39"></div>
+            <div class="loan-repayment-details"></div>
+            <div class="value"></div>
+            <div class="print-option"></div>
+            <img class="print-icon" src="print-icon.png" />
+            <div class="_100-000-00">$ 100 000.00</div>
+            <div class="loan-my-m-ovember-deal">Loan: “My MOvember” Deal</div>
+            <div class="date"></div>
+            <div class="_06-04-2023">06/04/2023</div>
+          </div>
+        </div>
+      </div>
+      <div class="chat-area">
+        <div class="text-input">
+          <div class="loan-repayment-application"></div>
+          <div class="text-input-contols">
+            <div class="loan-repayment-application2"></div>
+            <div class="clear-button">
+              <div class="clear">Clear</div>
+            </div>
+            <div class="send-button">
+              <div class="send">Send</div>
+            </div>
+          </div>
+          <div class="text-input-display"></div>
+        </div>
+        <div class="chat-display">
+          <div class="chat-display2"></div>
+          <div class="chat2">Chat</div>
+          <div class="chat-container">
+            <div class="conversation-display"></div>
+            <div class="date-input">
+              <div class="frame-39"></div>
+              <div class="date2">Date:</div>
+            </div>
+            <div class="converastion-title-input">
+              <div class="frame-39"></div>
+              <div class="title">Title:</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="profile-display">
+        <div class="profile-display2"></div>
+        <div class="quick-details-display"></div>
+        <div class="account-details">Account Details</div>
+        <div class="profile-name">
+          <div class="profile-name2"></div>
+          <div class="name-surname">name &amp; surname</div>
+        </div>
+        <div class="profile-image"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
