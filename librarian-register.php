25da54bbcb0a379e263e8bcdb9026fe60b80af51
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 12:29:00 +0200

    Update librarian-register.php

--- librarian-register.php
+++ librarian-register.php
@@ -10,7 +10,75 @@
     </style>
 </head>
 <body>
-    
+    <div class="librarian-register">
+  <div class="navbar-area">
+    <div class="navbar-area2"></div>
+    <div class="nav-bar">
+      <div class="navbar"></div>
+      <div class="navbar-center">
+        <div class="navbar-site-title"></div>
+        <div class="library-app">LIBRARY APP</div>
+        <div class="navbar-center2"></div>
+      </div>
+    </div>
+  </div>
+  <div class="body-container">
+    <div class="body-display">
+      <div class="container"></div>
+      <div class="login-area">
+        <div class="rectangle-81"></div>
+        <div class="form-area">
+          <div class="form">
+            <div class="rectangle-83"></div>
+            <div class="submit-button">
+              <div class="submit-button2"></div>
+              <div class="submit">SUBMIT</div>
+            </div>
+            <div class="form-data-entry">
+              <div class="confirm-password">
+                <div class="confirm-password-entry"></div>
+                <div class="confrim-password">Confrim Password</div>
+              </div>
+              <div class="password">
+                <div class="password-entry"></div>
+                <div class="password2">Password</div>
+              </div>
+              <div class="province">
+                <div class="province-entry"></div>
+                <div class="province2">Province</div>
+              </div>
+              <div class="email">
+                <div class="email-entry"></div>
+                <div class="email2">Email</div>
+              </div>
+              <div class="contanct-number">
+                <div class="contact-number-entry"></div>
+                <div class="contact-number">Contact Number</div>
+              </div>
+              <div class="librarian-code">
+                <div class="librarian-code-entry"></div>
+                <div class="librarian-code2">Librarian Code</div>
+              </div>
+              <div class="librarian-surname">
+                <div class="librarian-surname-entry"></div>
+                <div class="librarian-surname2">Librarian Surname</div>
+              </div>
+              <div class="librarian-name">
+                <div class="librarian-name-enrty"></div>
+                <div class="librarian-name2">Librarian Name</div>
+              </div>
+            </div>
+          </div>
+          <div class="form-header">
+            <div class="rectangle-82"></div>
+            <div class="sign-up">SIGN UP</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 	
 	
 </body>
