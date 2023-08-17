8368ff965ce4c69b72756d0bfda9deb680886050
SwaggyD761 <70922045+SwaggyD761@users.noreply.github.com>
2023-08-17 11:30:51 +0200

    Add files via upload

---
+++ librarian/include_files/include_navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css"> <!-- Link to an external CSS file -->
+    <style>
+        /* Inline CSS styles can also be defined here */
+    </style>
+</head>
+<body>
+    
+	
+	
+</body>
+</html>
